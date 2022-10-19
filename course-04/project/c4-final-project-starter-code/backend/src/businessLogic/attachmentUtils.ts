724c5af1ed372371e4db885a6e974f864fe8a33e
Olajire Dominic <lerryjay45@gmail.com>
2022-10-19 16:42:13 +0100

    Updated project to meet review feedback

--- course-04/project/c4-final-project-starter-code/backend/src/businessLogic/attachmentUtils.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/businessLogic/attachmentUtils.ts
@@ -1,11 +1,11 @@
 import * as AWS from 'aws-sdk'
-// import * as AWSXRay from 'aws-xray-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
 
-// const XAWS = AWSXRay.captureAWS(AWS)
+const XAWS = AWSXRay.captureAWS(AWS)
 export class AttachmentUtils {
   s3BucketName:string = process.env.ATTACHMENT_S3_BUCKET;
   createAttachmentPresignedUrl = (todoId: string) => {
-    const s3 = new AWS.S3({
+    const s3 = new XAWS.S3({
       signatureVersion: 'v4' // Use Sigv4 algorithm
     })
     return s3.getSignedUrl('putObject', { // The URL will allow to perform the PUT operation
